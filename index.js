c857a0b9149679ce453b81fc24131d9cfe398a7a
Riku Rouvila <riku.rouvila@gmail.com>
2021-08-30 18:55:23 +0300

    add new dashboard

--- index.js
+++ index.js
@@ -3,19 +3,19 @@ const puppeteer = require("puppeteer");
 const MINUTE = 60 * 1000;
 
 const sites = [
-  { url: "http://localhost:3000", showFor: MINUTE * 10 },
+  { url: "http://localhost:3001", showFor: MINUTE * 10 },
   { url: "https://yle.fi/", showFor: MINUTE * 1 },
   { url: "http://localhost:3000", showFor: MINUTE * 10 },
-  { url: "https://rare.fi", showFor: MINUTE * 1 },
+  { url: "https://www.bouncingdvdlogo.com/", showFor: MINUTE * 1 },
   { url: "http://localhost:3000", showFor: MINUTE * 10 },
   { url: "http://v1.windows93.net/", showFor: MINUTE * 3 },
-  { url: "http://localhost:3000", showFor: MINUTE * 10 },
+  { url: "http://localhost:3001", showFor: MINUTE * 10 },
   { url: "http://www.nyan.cat/", showFor: MINUTE * 3 },
-  { url: "https://hs.fi/", showFor: MINUTE * 5 },
-  { url: "http://localhost:3000", showFor: MINUTE * 10 },
+  { url: "https://www.bouncingdvdlogo.com/", showFor: MINUTE * 5 },
+  { url: "http://localhost:3001", showFor: MINUTE * 10 },
   { url: "https://cat-bounce.com/", showFor: MINUTE * 5 },
   { url: "http://www.patience-is-a-virtue.org/", showFor: MINUTE * 5 },
-  { url: "http://localhost:3000", showFor: MINUTE * 10 },
+  { url: "http://localhost:3001", showFor: MINUTE * 10 },
   { url: "https://www.zoomquilt.org/", showFor: MINUTE * 5 },
 ];
 
